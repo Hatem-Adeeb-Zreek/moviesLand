a07c27264f80ca6d4fabf7ef8789a5164c5e25e8
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2021-05-31 15:51:29 +0200

    setup the MovieDB API and fetch it to the Landing Page

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,9 @@
 //SERVER ROUTES
-export const USER_SERVER = '/api/users';
+export const USER_SERVER = "/api/users";
+
+// MovieDB API config
+export const API_KEY = "844dba0bfd8f3a4f3799f6130ef9e335";
+export const API_URL = "https://api.themoviedb.org/3/";
+
+// Images config from MovieDB
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { API_URL, API_KEY } from "../../Config";
 
 function LandingPage() {
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        fetchMovies(endpoint);
+    }, []);
+
+    // fetch movies function from MovieDB API
+    const fetchMovies = (endpoint) => {
+        fetch(endpoint)
+            .then((result) => result.json())
+            .then((result) => console.log(result))
+            .catch((error) => console.error("Error:", error));
+    };
     return (
         <>
             <div className="app"></div>
