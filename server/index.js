0f17b61b2c116d1e78e458068a975de5133243ac
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2021-06-03 16:34:23 +0200

    create comment route

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/users", require("./routes/users"));
+app.use("/api/comment", require("./routes/comment"));
 app.use("/api/favorite", require("./routes/favorite"));
 
 app.use("/uploads", express.static("uploads"));
