05aa95115cd6cff68a8a00df6f9cd25dec656259
Hatem-Adeeb-Zreek <hatem.zreek88@gmail.com>
2021-06-03 23:40:52 +0200

    build version

---
+++ client/build/precache-manifest.8b8a951dbe40a1b14c10072ebef80c85.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687d33caeea7dad7f604bed223e22729",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00dfe878d5746a50034d",
+    "url": "/static/css/2.c4c447c0.chunk.css"
+  },
+  {
+    "revision": "4c38b2e0c88736440f31",
+    "url": "/static/css/main.68739c66.chunk.css"
+  },
+  {
+    "revision": "00dfe878d5746a50034d",
+    "url": "/static/js/2.1d158560.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/static/js/2.1d158560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c38b2e0c88736440f31",
+    "url": "/static/js/main.39608f49.chunk.js"
+  },
+  {
+    "revision": "285214a8420ba72e96a1",
+    "url": "/static/js/runtime-main.51fcc8e4.js"
+  },
+  {
+    "revision": "19a331769556174cfca4913c53ea1c86",
+    "url": "/static/media/Logo.19a33176.png"
+  }
+]);
